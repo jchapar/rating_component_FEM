dace02a33abffbf44dd1146cfc9844331ff19bd5
josechaparro <josechaparro.dev@gmail.com>
2022-07-03 11:55:38 -0700

    markup done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: '#FB7718',
+        lightGrey: '#959eac',
+        mediumGrey: '#7C8798',
+        darkBlue: '#252D37',
+        veryDarkBlue: '#121417',
+        dark: '#151519',
+        iconBg: '#262f38',
+      },
+      fontFamily: {
+        sans: ['overpass', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
+};
